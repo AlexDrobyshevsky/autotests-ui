b1b06da8ed263c39ce80ecec00c86ce24f05e527
AlexDrobyshevsky <alexdrobyshevskiy@gmail.com>
2026-08-19 00:41:37 +0300

    Интеграция Pytest и Playwright.Практика написания автотестов

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -13,5 +13,10 @@ class TestUserLogin:
 def test_assert_positive_case():
     assert 2 + 2 == 4
 
+
 def test_assert_negative_case():
     assert 2 + 2 == 3, 'не верно!'
+
+
+def test_assert_positive_case2():
+    assert 4 + 5 == 9
