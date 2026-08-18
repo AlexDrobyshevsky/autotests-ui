a36ad3e4bc84a51535df7782efd3fd957a3aa6a1
AlexDrobyshevsky <alexdrobyshevskiy@gmail.com>
2026-08-18 21:50:26 +0300

    Введение в Pytest

---
+++ test_pytest.py
@@ -0,0 +1,17 @@
+def test_user_login():
+    print("hello")
+
+
+class TestUserLogin:
+    def test_1(self):
+        print("hello1")
+
+    def test_2(self):
+        print("hello2")
+
+
+def test_assert_positive_case():
+    assert 2 + 2 == 4
+
+def test_assert_negative_case():
+    assert 2 + 2 == 3, 'не верно!'
