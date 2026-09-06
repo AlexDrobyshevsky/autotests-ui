595f2229aa4ea11e975c701b529fa4b8311f4631
AlexDrobyshevsky <alexdrobyshevskiy@gmail.com>
2026-09-07 01:21:02 +0300

    Знакомство с PageObject.Практика работы с Page Object

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,9 +1,20 @@
 import pytest
 from playwright.sync_api import Page
-
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
 
 
 @pytest.fixture
 def login_page(chromium_page: Page):
     return LoginPage(page=chromium_page)
+
+
+@pytest.fixture
+def registration_page(chromium_page: Page):
+    return RegistrationPage(page=chromium_page)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page):
+    return DashboardPage(page=chromium_page)
